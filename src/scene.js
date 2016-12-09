1b4d015f3fe9b62196fd6e34f17da5b3891ecd13
Kyle <kyle@flux.io>
2016-12-08 17:21:51 -0800

    ID based selection

--- src/scene.js
+++ src/scene.js
@@ -96,7 +96,16 @@ Scene.prototype.createTree = function (entities) {
             data.push(this.createLayer(entity));
         }
     } else {
-        data.push({"text" : "Not a scene"});
+        for (i=0;i<entities.length;i++) {
+            var entity = entities[i];
+            if (entity == null || typeof entity != 'object' || !entity.primitive) continue;
+            if (!entity.id) {
+                entity.id = entity.fluxId;
+                if (!entity.id) continue;
+            }
+            this._scene[entity.id] = entity;
+            data.push(createTreeNode(entity));
+        }
     }
     this.tree = $.jstree.create(this._treeDiv,tree);
 
@@ -115,16 +124,11 @@ Scene.prototype.focus = function (obj) {
 Scene.prototype.onSelected = function (event) {
     this._updateViewport = false;
     this.tree.deselect_all();
-    if (event.selection) {
-        var keys = Object.keys(event.selection);
-        for (var i=0;i<keys.length;i++) {
-            var obj = event.selection[keys[i]];
-            if (!obj) continue;
-            var id = obj.userData.id;
-            if (obj.parent && obj.parent.userData.primitive === 'instance')
-                id = obj.parent.userData.id;
-            this.tree.select_node(id);
-        }
+    var map = this._vp.getObjectMap();
+    var selection = this._vp.getSelection();
+    for (var i=0;i<selection.length;i++) {
+        var id = selection[i];
+        this.tree.select_node(id);
     }
     this._updateViewport = true;
 };
@@ -132,13 +136,6 @@ Scene.prototype.onSelected = function (event) {
 // update viewport selection from tree changes
 Scene.prototype.updateSelected = function (e, data) {
     if (!this._updateViewport) return;
-    var selectionList = [];
-    for (var i=0;i<data.selected.length; i++) {
-        var id = data.selected[i];
-        var obj = this._objectMap[id];
-        if (!obj) continue;
-        selectionList.push(obj);
-    }
-    this._vp.setSelection(selectionList);
+    this._vp.setSelection(data.selected);//selectionList
     this._vp.render();
 }
